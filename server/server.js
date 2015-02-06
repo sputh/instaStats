32aa7dfead5fc318e1b14c2bd9781f9a544ef351
sputh <samantha.puth@gmail.com>
2015-02-06 15:59:55 -0800

    Instantiate server to serve up simple html page

--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 
 var app = express();
 var port = process.env.PORT || 4568;
 
-app.set('public', __dirname + '/public');
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/../public'));
 
-// Cookie & Session
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/*', function(req, res) {
+    res.redirect('/');
+});
+
+// // Cookie & Session
 app.use(cookieParser());
 app.use(session({secret: '123Secret', cookie: {maxAge: 60000}}));
 
