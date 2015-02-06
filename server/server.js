248ecdf00d3ce41ce9c180a0ebc2cbe186a4dc89
sputh <samantha.puth@gmail.com>
2015-02-06 15:15:51 -0800

    create Cookie and sessions for persistant user experience

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
 var app = express();
 var port = process.env.PORT || 4568;
 
+app.set('public', __dirname + '/public');
 app.use(express.static(__dirname + '/public'));
+
+// Cookie & Session
+app.use(cookieParser());
+app.use(session({secret: '123Secret', cookie: {maxAge: 60000}}));
 
 app.listen(port, function () {
 	console.log('Server now listening on port ' + port);
