ebdb79572ee50295f64fb2fba2ecc66301ca56ae
sputh <samantha.puth@gmail.com>
2015-02-06 15:02:10 -0800

    Instantiate Basic Server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-server.js
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 4568;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function () {
+	console.log('Server now listening on port ' + port);
+});
